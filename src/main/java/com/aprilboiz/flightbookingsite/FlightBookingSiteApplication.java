c8f72f85380716ae4208a88c0890a4b2b4f37669
Tuan Anh <phantuananh0202@gmail.com>
2025-03-25 10:14:17 +0700

    Initial source

---
+++ src/main/java/com/aprilboiz/flightbookingsite/FlightBookingSiteApplication.java
@@ -0,0 +1,13 @@
+package com.aprilboiz.flightbookingsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightBookingSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aprilboiz/flightbookingsite/FlightBookingSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprilboiz.flightbookingsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
